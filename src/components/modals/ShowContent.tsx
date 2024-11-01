0969eb1ade4001e3ced06df2ca9a288da65b7db2
Mohamed Magdy <m.magdy@limitlesslabs.ai>
2024-11-01 20:18:05 +0200

    add text in show modal

--- src/components/modals/ShowContent.tsx
+++ src/components/modals/ShowContent.tsx
@@ -31,49 +31,57 @@ export function ShowContentModal({
       <View className="px-4 py-6">
         <Text className="text-2xl font-semibold mb-4">Show</Text>
         <View className="mb-4">
-          {["Requests", "Inventory"].map((option, index) => (
+          {[
+            { option: "Requests", text: "Find Requests from other brokers" },
+            { option: "Inventory", text: "Find properties listed" },
+          ].map((item, index) => (
             <TouchableOpacity
               key={index}
-              className={`flex-row items-center py-3 ${
-                option === "Requests" ? "bg-primary_50" : "bg-secondary_50"
+              className={`py-3 ${
+                item.option === "Requests" ? "bg-primary_50" : "bg-secondary_50"
               } mb-3 rounded-lg p-2`}
               onPress={() =>
                 handleOptionChange(
-                  option.toLowerCase().replace(" ", "") as FilterOption
+                  item.option.toLowerCase().replace(" ", "") as FilterOption
                 )
               }
             >
-              <View
-                className={`w-5 h-5 rounded-full border-2 mr-3 ${
-                  selectedOption === option.toLowerCase().replace(" ", "")
-                    ? "bg-blue-500 border-blue-500"
-                    : "border-gray-300"
-                } justify-center items-center`}
-              >
-                {selectedOption === option.toLowerCase().replace(" ", "") && (
-                  <View className="w-2 h-2 rounded-full bg-white" />
-                )}
+              <View className="flex-row items-center">
+                <View
+                  className={`w-5 h-5 rounded-full border-2 mr-3 ${
+                    selectedOption ===
+                    item.option.toLowerCase().replace(" ", "")
+                      ? "bg-blue-500 border-blue-500"
+                      : "border-gray-300"
+                  } justify-center items-center`}
+                >
+                  {selectedOption ===
+                    item.option.toLowerCase().replace(" ", "") && (
+                    <View className="w-2 h-2 rounded-full bg-white" />
+                  )}
+                </View>
+                <Text
+                  className={`text-base ${
+                    item.option === "Requests"
+                      ? "text-primary_500"
+                      : "text-secondary_500"
+                  }`}
+                >
+                  {item.option}
+                </Text>
               </View>
               <Text
-                className={`text-base ${
-                  option === "Requests"
+                className={`pl-8 ${
+                  item.option === "Requests"
                     ? "text-primary_500"
                     : "text-secondary_500"
                 }`}
               >
-                {option}
+                {item.text}
               </Text>
             </TouchableOpacity>
           ))}
         </View>
-        <TouchableOpacity
-          className="bg-blue-500 py-3 rounded-lg"
-          onPress={handleApply}
-        >
-          <Text className="text-white text-center text-base font-semibold">
-            Apply
-          </Text>
-        </TouchableOpacity>
       </View>
     </ModalContainer>
   );
